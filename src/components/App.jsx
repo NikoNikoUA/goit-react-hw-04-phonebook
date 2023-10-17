c4deb7257049330052f7a1ee82b9bdbc675e4659
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-10-17 16:15:29 +0300

    partial refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { nanoid } from 'nanoid'
 import { GiRotaryPhone} from 'react-icons/gi'
@@ -18,93 +18,78 @@ Notify.init({
   cssAnimationStyle: 'zoom',
 });
 
-class App extends Component {
-state = {
-  contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
-  filter: '',
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {const savedContacts = localStorage.getItem('contacts');
+      if (savedContacts !== null) {
+        return JSON.parse(savedContacts);
     }
+  return [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ] 
+  })
 
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts !== null) {
-      this.setState({ contacts: JSON.parse(savedContacts) });
-    }
-  }
+  const [filter, setFilter] = useState('')
 
-  componentDidUpdate(prevProps, prevState) {
-    const currentContacts = this.state.contacts;
-    const previousContacts = prevState.contacts;
-    if (currentContacts !== previousContacts) {
-      localStorage.setItem('contacts', JSON.stringify(currentContacts));
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts), [contacts]);
+  })
+     
+
+  const onRemoveContact = (contactId) => {
+    setContacts(prevState => ({
+        ...prevState,
+        contacts: prevState.filter(contact => contact.id !== contactId)
+      }))
+      Notify.success('The contact has been successfully removed');
     }
-  }
   
-  onRemoveContact = (contactId) => {
-     this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId)
-    }))
-    Notify.success('The contact has been successfully removed');
-  }
-  
-  onAddingContact = ({ name, number }) => {
-    const { contacts } = this.state;
-    if (contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
+   const onAddingContact = ({ name, number }) => {
+      if (contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
         return Notify.info(`${name} is already among your contacts`);
-    }
+      }
 
-    console.log({name, number});
-    const contact = {
-      name,
-      number,
-      id: nanoid(),
-    }
+      console.log({ name, number });
+      const contact = {
+        name,
+        number,
+        id: nanoid(),
+      }
 
-    this.setState(prevState => {
-      return {
-        contacts: [...prevState.contacts, contact],
-      };
-    } );
-    Notify.success(`${name} has been successfully added to your contacts`);
-
-      
+      setContacts(prevState => {
+        return {
+          ...prevState,
+          contacts: [...prevState, contact],
+        };
+      });
+      Notify.success(`${name} has been successfully added to your contacts`);
+     
   }
   
-  onFilterChange = event => {
-    this.setState({ filter: event.currentTarget.value })
+    const onFilterChange = event => {
+      setFilter(event.currentTarget.value);
   }
-
-  onFilteringContacts = () => {
-    const { contacts, filter } = this.state;
-    const normalizedFilter = filter.toLowerCase()
-    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
-
-  }
-    
-  render() {
-
-    const { filter, contacts } = this.state;
   
-    const filteredContacts = this.onFilteringContacts();
+ const filteredContacts = () => {
+      const normalizedFilter = filter.toLowerCase()
+      return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
 
-   return (
-    <Container>
-      <FormContainer>
-         <MainHeading>Phonebook <GiRotaryPhone/></MainHeading>
-         <ContactForm onSubmit={this.onAddingContact} />
-        </FormContainer>
-<StatisticsContainer>
-  <ContactsHeading>Contacts</ContactsHeading>
-       <Filter value={filter} onChange={ this.onFilterChange} />
-      {contacts.length ? (<ContactList contacts={filteredContacts} onRemoveContact={this.onRemoveContact} />) : (<NoContactsText>There are no contacts in your phoneboook</NoContactsText>)}
-    </StatisticsContainer>
-       </Container>
-  )
-}
-}
+    }
 
-export default App;
+
+      return (
+        <Container>
+          <FormContainer>
+            <MainHeading>Phonebook <GiRotaryPhone /></MainHeading>
+            <ContactForm onSubmit={onAddingContact} />
+          </FormContainer>
+          <StatisticsContainer>
+            <ContactsHeading>Contacts</ContactsHeading>
+            <Filter value={filter} onChange={onFilterChange} />
+            {contacts.length ? (<ContactList contacts={filteredContacts} onRemoveContact={onRemoveContact} />) : (<NoContactsText>There are no contacts in your phoneboook</NoContactsText>)}
+          </StatisticsContainer>
+        </Container>
+      )
+  }
