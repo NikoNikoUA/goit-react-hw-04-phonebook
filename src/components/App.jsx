e0b4d448c6ca52c2cd81328f60c8d7463eed451f
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-10-18 16:07:33 +0300

    updated code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,30 +23,29 @@ export const App = () => {
       if (savedContacts !== null) {
         return JSON.parse(savedContacts);
     }
-  return [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ] 
+    return [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]; 
   })
 
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts), [contacts]);
-  })
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
      
 
   const onRemoveContact = (contactId) => {
-    setContacts(prevState => ({
-        ...prevState,
-        contacts: prevState.filter(contact => contact.id !== contactId)
-      }))
+    setContacts(prevState => 
+        prevState.filter(contact => contact.id !== contactId)
+      )
       Notify.success('The contact has been successfully removed');
     }
   
-   const onAddingContact = ({ name, number }) => {
+   const onAddingContact = ({ name, number} ) => {
       if (contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
         return Notify.info(`${name} is already among your contacts`);
       }
@@ -58,12 +57,7 @@ export const App = () => {
         id: nanoid(),
       }
 
-      setContacts(prevState => {
-        return {
-          ...prevState,
-          contacts: [...prevState, contact],
-        };
-      });
+      setContacts(prevState =>[...prevState, contact]);
       Notify.success(`${name} has been successfully added to your contacts`);
      
   }
@@ -88,7 +82,7 @@ export const App = () => {
           <StatisticsContainer>
             <ContactsHeading>Contacts</ContactsHeading>
             <Filter value={filter} onChange={onFilterChange} />
-            {contacts.length ? (<ContactList contacts={filteredContacts} onRemoveContact={onRemoveContact} />) : (<NoContactsText>There are no contacts in your phoneboook</NoContactsText>)}
+            {contacts.length ? (<ContactList contacts={filteredContacts()} onRemoveContact={onRemoveContact} />) : (<NoContactsText>There are no contacts in your phoneboook</NoContactsText>)}
           </StatisticsContainer>
         </Container>
       )
